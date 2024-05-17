e3596024518c99c825895b701d2bb5d082e15de3
MockingJaySE <160509773+MockingJaySE@users.noreply.github.com>
2024-05-17 23:06:41 +0530

    1-project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "you hit the signup enspoint",
+  });
+};
+
+export const login = async (req, res) => {
+  res.json({
+    data: "you hit the login enspoint",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    data: "you hit the logout enspoint",
+  });
+};
