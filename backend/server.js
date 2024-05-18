b6f86a9c68f73e1b19c540bc2ee1035804e5e31a
MockingJaySE <160509773+MockingJaySE@users.noreply.github.com>
2024-05-18 23:42:23 +0530

    1-auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -8,7 +10,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// console.log(process.env.MONGO_URI);
+app.use(express.json()); //to pass request to body
+app.use(express.urlencoded({ extended: true })); //to pass form data(urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
